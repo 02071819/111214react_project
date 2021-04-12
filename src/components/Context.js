1c94e731dd8cbe7b2c7c687e6b73a5c1965d72ae
02071819 <54053565+02071819@users.noreply.github.com>
2021-04-12 18:16:01 +0800

    Add files via upload

---
+++ src/components/Context.js
@@ -0,0 +1,157 @@
+import React, { Component } from 'react'
+
+export const DataContext = React.createContext();
+
+export class DataProvider extends Component {
+
+    state = {
+        products: [
+            {
+                "_id": "1",
+                "title": "Nike Shoes 01",
+                "src": "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/61792ae3-ee00-4915-86f0-9392622b12bd/lebron-witness-5-ep-%E7%B1%83%E7%90%83-RRFJkZ.png",
+                "description": "UI/UX designing, html css tutorials",
+                "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+                "price": 23,
+                "colors": ["red", "black", "crimson", "teal"],
+                "count": 1
+            },
+            {
+                "_id": "2",
+                "title": "Nike Shoes 02",
+                "src": "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/qmmwhyelq5qegzvh8qu4/air-monarch-4-%E7%94%B7%E6%AC%BE%E8%A8%93%E7%B7%B4-cc3XbD.jpg",
+                "description": "UI/UX designing, html css tutorials",
+                "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+                "price": 19,
+                "colors": ["red", "crimson", "teal"],
+                "count": 1
+            },
+            {
+                "_id": "3",
+                "title": "Nike Shoes 03",
+                "src": "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4e69c9d0-e77b-4844-b94c-c109a09a0cb8/kyrie-flytrap-3-ep-%E7%B1%83%E7%90%83-0qb5vw.jpg",
+                "description": "UI/UX designing, html css tutorials",
+                "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+                "price": 50,
+                "colors": ["lightblue", "white", "crimson", "teal"],
+                "count": 1
+            },
+            {
+                "_id": "4",
+                "title": "Nike Shoes 04",
+                "src": "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/b471b02a-767d-4ac0-bc96-c7a8c1f117e7/lebron-17-graffiti-%E7%B1%83%E7%90%83-XNGQVG.jpg",
+                "description": "UI/UX designing, html css tutorials",
+                "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+                "price": 15,
+                "colors": ["orange", "black", "crimson", "teal"],
+                "count": 1
+            },
+            {
+                "_id": "5",
+                "title": "Nike Shoes 05",
+                "src": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkvghqaiF0Bg6dhVkJ7p_1xkGscdvzt07mXg&usqp=CAU",
+                "description": "UI/UX designing, html css tutorials",
+                "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+                "price": 10,
+                "colors": ["orange", "black", "crimson", "teal"],
+                "count": 1
+            },
+            {
+                "_id": "6",
+                "title": "Nike Shoes 06",
+                "src": "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/daa4dd90-8d97-42e2-bdce-547d5e856891/air-zoom-tempo-next-flyease-%E7%94%B7%E6%AC%BE%E8%B7%91-40pJSQ.png",
+                "description": "UI/UX designing, html css tutorials",
+                "content": "Welcome to our channel Dev AT. Here you can learn web designing, UI/UX designing, html css tutorials, css animations and css effects, javascript and jquery tutorials and related so on.",
+                "price": 17,
+                "colors": ["orange", "black", "crimson", "teal"],
+                "count": 1
+            }
+        ],
+        cart: [],
+        total: 0
+    };
+
+    addCart = (id) => {
+        const { products, cart } = this.state;
+        const check = cart.every(item => {
+            return item._id !== id //連續點擊=false
+        })
+        if (check) {
+            const data = products.filter(product => {
+                return product._id === id
+            })
+            this.setState({ cart: [...cart, ...data] })
+        } else {
+            alert("該商品已被加入至購物車。")
+        }
+    };
+
+    reduction = id => {
+        const { cart } = this.state;
+        cart.forEach(item => {
+            if (item._id === id) {
+                item.count === 1 ? item.count = 1 : item.count -= 1;
+            }
+        })
+        this.setState({ cart: cart });
+        this.getTotal();
+    };
+
+    increase = id => {
+        const { cart } = this.state;
+        cart.forEach(item => {
+            if (item._id === id) {
+                item.count += 1;
+            }
+        })
+        this.setState({ cart: cart });
+        this.getTotal();
+    };
+
+    removeProduct = id => {
+        if(window.confirm("您確定刪除這項產品嗎?")){
+            const {cart} = this.state;
+            cart.forEach((item, index) =>{
+                if(item._id === id){
+                    cart.splice(index, 1)
+                }
+            })
+            this.setState({cart: cart});
+            this.getTotal();
+        }
+    };
+
+    getTotal = () =>{
+        const{cart} = this.state;
+        const res = cart.reduce((prev, item) => {
+            return prev + (item.price * item.count);
+        },0)
+        this.setState({total: res})
+    };
+
+    componentDidUpdate(){
+        localStorage.setItem('dataCart', JSON.stringify(this.state.cart))
+        localStorage.setItem('dataTotal', JSON.stringify(this.state.total))
+    };
+
+    componentDidMount(){
+        const dataCart = JSON.parse(localStorage.getItem('dataCart'));
+        if(dataCart !== null){
+            this.setState({cart: dataCart});
+        }
+        const dataTotal = JSON.parse(localStorage.getItem('dataTotal'));
+        if(dataTotal !== null){
+            this.setState({total: dataTotal});
+        }
+    };
+
+    render() {
+        const { products, cart, total } = this.state;
+        const { addCart, reduction, increase, removeProduct, getTotal } = this;
+        return (
+            <DataContext.Provider value={{ products, addCart, cart, reduction, increase, removeProduct, total, getTotal }}>
+                {this.props.children}
+            </DataContext.Provider>
+        )
+    }
+}
